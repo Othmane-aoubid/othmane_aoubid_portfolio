f0d780f441784c35e0524d8941841011961b8481
othmane_aoubid <othmane.aoubid.yosobox@gmail.com>
2024-08-02 21:23:57 +0100

    setting up the information, first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="portfolio__container container">this is othmane aoubid portfolio website</div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
